4986e8e8e86691cb5b3ffa1866570db45c65f792
Rafael Rocha Vano <rafa.vano10@gmail.com>
2024-10-08 20:24:03 -0300

    Adicionando evento de clique com JavaScript

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
 }); 
 const meuTitulo = document.querySelector('h1'); 
 meuTitulo.textContent = 'Título modificado pelo JavaScript!'; 
+document.addEventListener('DOMContentLoaded', function(){ 
+}); 
+const minhaLista = document.querySelector('ul'); 
+minha Lista.addEventListener('click', function() { 
+}); 
+alert('Você clicou na lista!'); 
